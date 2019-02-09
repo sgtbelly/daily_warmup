11b66bd2a169afe07e17647863c3321e6630236d
James Denton <sgtbelly@gmail.com>
2019-02-08 22:52:19 -0800

    DEC 19

---
+++ 22-DEC-18/stack.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Stack
+class Stack {
+
+    constructor(){
+        this.items = [];
+
+        this.items.push(4);
+        this.items.push(3);
+        this.items.push(2);
+        this.items.push(5);
+        this.items.push(1);
+    }
+
+}
+
+
+console.log(Stack);
+
+
