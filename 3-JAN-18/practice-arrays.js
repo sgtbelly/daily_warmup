51cc4d77d9d8054e8e72984af06bcf7c8bc60c59
James Denton <sgtbelly@gmail.com>
2019-02-16 18:12:25 -0800

    3-jan-18

--- 3-JAN-18/practice-arrays.js
+++ 3-JAN-18/practice-arrays.js
@@ -1,20 +1,64 @@
-'use strict';
+// Warmup - Arrays
 
-let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-forloop(){
-    for (i; i < arr1.length; i++) {
-        console.log(arr[i]);
+forLoop = (array) => {
+    for(let i = 0; i < array.length; i++) {
+        console.log('for loop index', i);
     }
 }
 
-whileloop(){
-    while (i > 0){
-        console.log(arr1[i]);
-        i++}
+forLoop([1,2,3,4,5,6,7,8,9,10]);
+
+whileLoop = (array) => {
+    let i = 0;
+    while (i < array.length) {
+        console.log('while loop index:', array[i]);
+        i++
+    }
 }
 
-.map()
+whileLoop([1,2,3,4,5,6,7,8,9,10]);
 
+map = (array) => {
+    let mapped = array.map(function(index) {
+        return index + 10;
+    });
 
+    // expect output: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    console.log('mapped', mapped);
+}
 
+map([1,2,3,4,5,6,7,8,9,10]);
+
+filter = (array) => {
+    let filter = array.filter(number => number > 3);
+    // expect output: [4,5,6,7,8,9,10]
+    console.log('filter', filter);
+}
+
+filter([1,2,3,4,5,6,7,8,9,10]);
+
+reduce = (array) => {
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    // expect output: 55
+    console.log(array.reduce(reducer));
+}
+
+reduce([1,2,3,4,5,6,7,8,9,10]);
+
+// Warmup - Objects
+
+const people = ['Kookla','Fran','Ollie'];
+let newPeople = ['Oldie', ...people, 'Garfield'];
+
+const stuff = {
+    tv: 'huge',
+    radio: 'old',
+    toothbrush: 'frayed',
+    cars: ['Toyota','Mazda']
+}
+
+let newStuff = {...stuff, cars:[...stuff.cars, "Ford"]};
+
+let state = {people, stuff};
+
+let newState = {...state, people: ['Oldie', ...people, 'Garfield'], stuff: {...stuff, cars:[...stuff.cars, 'Ford']}}
